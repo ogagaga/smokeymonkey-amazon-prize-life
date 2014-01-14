8491a458e7d5d808f5926c1d820749e64ccf1f07
ogagaga <y.ogagaga@gmail.com>
2014-01-14 16:07:32 +0900

    Fixed to be able to list the data of items.json

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,3 +4,11 @@
 
 var smokeymonkeyControllers = angular.module('smokeymonkeyControllers', []);
 
+smokeymonkeyControllers.controller('ItemListCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $http.get('items/items.json').success(function(data) {
+      $scope.items = data;
+    });
+
+    $scope.orderProp = 'no';
+  }]);
