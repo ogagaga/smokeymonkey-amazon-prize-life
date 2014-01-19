85b0908b13f47622a3964491f8e4cf3d228c1f27
Yutaka Ogasawara <y.ogagaga@gmail.com>
2014-01-19 19:38:18 +0900

    Added Twitter API settings and api sample code

--- scripts/twitter_search.rb
+++ scripts/twitter_search.rb
@@ -1,3 +1,35 @@
 #!/bin/env ruby
 #encoding:UTF-8
+require "yaml"
 require "twitter"
+
+config_file = File.join('..', 'config', 'twitter_api_conf.yml')
+#puts config_file
+TWITTER_CONFIG = YAML.load(File.open(config_file))
+#puts "#{TWITTER_CONFIG.inspect}"
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key = TWITTER_CONFIG['twitter']['consumer_key']
+  config.consumer_secret = TWITTER_CONFIG['twitter']['consumer_secret']
+  config.oauth_token = TWITTER_CONFIG['twitter']['oauth_token']
+  config.oauth_token_secret = TWITTER_CONFIG['twitter']['oauth_token_secret']
+end
+
+# client.update("Testツイート:I'm tweeting with @gem!")
+
+client.search("#hokkathon -rt", :count => 3, :result_type => "recent").take(3).collect do |tweet|
+  puts "#{tweet.user.screen_name}: #{tweet.text}"
+end
+
+# # for Streaming
+# client = Twitter::Streaming::Client.new do |config|
+#   config.consumer_key = TWITTER_CONFIG['twitter']['consumer_key']
+#   config.consumer_secret = TWITTER_CONFIG['twitter']['consumer_secret']
+#   config.oauth_token = TWITTER_CONFIG['twitter']['oauth_token']
+#   config.oauth_token_secret = TWITTER_CONFIG['twitter']['oauth_token_secret']
+# end
+
+# topics = ["coffee", "tea"]
+# client.filter(:track => topics.join(",")) do |object|
+#   puts object.text if object.is_a?(Twitter::Tweet)
+# end
