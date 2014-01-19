7abd4fc8d6e9bc9adb1f4f1403df790a05de045a
Yutaka Ogasawara <y.ogagaga@gmail.com>
2014-01-19 20:15:41 +0900

    Created TwitterSearch Class

--- scripts/twitter_search.rb
+++ scripts/twitter_search.rb
@@ -3,33 +3,30 @@
 require "yaml"
 require "twitter"
 
-config_file = File.join('..', 'config', 'twitter_api_conf.yml')
-#puts config_file
-TWITTER_CONFIG = YAML.load(File.open(config_file))
-#puts "#{TWITTER_CONFIG.inspect}"
+class TwitterSearch
 
-client = Twitter::REST::Client.new do |config|
-  config.consumer_key = TWITTER_CONFIG['twitter']['consumer_key']
-  config.consumer_secret = TWITTER_CONFIG['twitter']['consumer_secret']
-  config.oauth_token = TWITTER_CONFIG['twitter']['oauth_token']
-  config.oauth_token_secret = TWITTER_CONFIG['twitter']['oauth_token_secret']
+  def initialize
+    config_file = File.join('..', 'config', 'twitter_api_conf.yml')
+    #puts config_file
+
+    twitter_config = YAML.load(File.open(config_file))
+    #puts "#{twitter_config.inspect}"
+
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = twitter_config['twitter']['consumer_key']
+      config.consumer_secret = twitter_config['twitter']['consumer_secret']
+      config.oauth_token = twitter_config['twitter']['oauth_token']
+      config.oauth_token_secret = twitter_config['twitter']['oauth_token_secret']
+    end
+  end
+
+  def search
+    @client.search("#hokkathon -rt", :count => 3, :result_type => "recent").take(3).collect do |tweet|
+      puts "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+  end
+
 end
 
-# client.update("Testツイート:I'm tweeting with @gem!")
-
-client.search("#hokkathon -rt", :count => 3, :result_type => "recent").take(3).collect do |tweet|
-  puts "#{tweet.user.screen_name}: #{tweet.text}"
-end
-
-# # for Streaming
-# client = Twitter::Streaming::Client.new do |config|
-#   config.consumer_key = TWITTER_CONFIG['twitter']['consumer_key']
-#   config.consumer_secret = TWITTER_CONFIG['twitter']['consumer_secret']
-#   config.oauth_token = TWITTER_CONFIG['twitter']['oauth_token']
-#   config.oauth_token_secret = TWITTER_CONFIG['twitter']['oauth_token_secret']
-# end
-
-# topics = ["coffee", "tea"]
-# client.filter(:track => topics.join(",")) do |object|
-#   puts object.text if object.is_a?(Twitter::Tweet)
-# end
+twitter_search = TwitterSearch.new
+twitter_search.search
