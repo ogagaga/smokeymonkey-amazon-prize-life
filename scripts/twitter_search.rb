d795c9c2b583588e5247104d4d3025d8096213a2
Yutaka Ogasawara <y.ogagaga@gmail.com>
2014-01-19 23:52:43 +0900

    Fixed search method and Add timeline method

--- scripts/twitter_search.rb
+++ scripts/twitter_search.rb
@@ -21,9 +21,25 @@ class TwitterSearch
   end
 
   def search
-    @client.search("#hokkathon -rt", :count => 3, :result_type => "recent", :include_entities => true).take(3).collect do |tweet|
+    # @client.search("#hokkathon -rt", :count => 3, :result_type => "recent", :include_entities => true).take(3).collect do |tweet|
+    @client.search("from:smokeymonkey #昼飯 -rt", :count => 3, :result_type => "recent", :include_entities => true).take(3).collect do |tweet|
       puts "#{tweet.id} :#{tweet.user.screen_name}: #{tweet.text} : #{tweet.created_at}"
       puts "#{tweet.user.profile_image_url}"
+    end
+  end
+
+  def collect_with_max_id(collection=[], max_id=nil, &block)
+    response = yield max_id
+    collection += response
+    response.empty? ? collection.flatten : collect_with_max_id(collection, response.last.id - 1, &block)
+  end
+
+  def get_all_tweets(user)
+    collect_with_max_id do |max_id|
+      options = {:count => 200, :include_rts => true}
+      options[:max_id] = max_id unless max_id.nil?
+      # @client.user_timeline(user, options)
+      @client.user_timeline(user, options)
     end
   end
 
@@ -31,3 +47,8 @@ end
 
 twitter_search = TwitterSearch.new
 twitter_search.search
+
+time_line = twitter_search.get_all_tweets("smokeymonkey")
+time_line.each do |tweet|
+  puts tweet.text
+end
