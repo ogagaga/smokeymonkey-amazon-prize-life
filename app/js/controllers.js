4f6be3accce26d64cb4f06c816935a801ccb184a
Yutaka Ogasawara <y.ogagaga@gmail.com>
2014-02-07 00:25:26 +0900

    Modified to read JSON data of meal

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,7 @@ var smokeymonkeyControllers = angular.module('smokeymonkeyControllers', []);
 
 smokeymonkeyControllers.controller('ItemListCtrl', ['$scope', '$http',
   function($scope, $http) {
-    $http.get('items/items.json').success(function(data) {
+    $http.get('items/smokeymonkey_meal_tweet.json').success(function(data) {
       $scope.items = data;
     });
 
