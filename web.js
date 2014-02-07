af310e90fed2650cfe106d69d07396dd268a97df
ogagaga <y.ogagaga@gmail.com>
2014-02-07 15:06:12 +0900

    Update package.json and web.js

--- web.js
+++ web.js
@@ -1,17 +1,17 @@
 var static = require('node-static');
- 
+
 //
-// Create a node-static server instance to serve the './app' folder
+// Create a node-static server instance to serve the './public' folder
 //
-var file = new(static.Server)('./app');
-var port = process.env.PORT || 5000;
+var file = new static.Server('./app');
+
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         //
         // Serve files!
         //
         file.serve(request, response);
-    });
-}).listen(port);
+    }).resume();
+}).listen(8080);
 
 
