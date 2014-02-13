bfe0919c496a9ab390f504af3280f2818c2e4e4d
ogagaga <y.ogagaga@gmail.com>
2014-02-13 13:40:09 +0900

    Closes #5 Set no image

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -8,6 +8,15 @@ smokeymonkeyControllers.controller('ItemListCtrl', ['$scope', '$http',
   function($scope, $http) {
     $http.get('items/smokeymonkey_meal_tweet.json').success(function(data) {
       $scope.items = data;
+      var getItems = $scope.items;
+      $scope.items = [];
+      angular.forEach(getItems, function(item) {
+        if (!item.imageUrl) {
+          item.imageUrl = "./images/no-image.JPG"
+          // console.dir(item);
+        }
+        $scope.items.push(item);
+      });
     });
 
     $scope.orderProp = 'id';
