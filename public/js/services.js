2169a7bcc37a4f4403b062bb9099783d13fc9616
Yutaka Ogasawara <y.ogagaga@gmail.com>
2014-06-14 15:55:26 +0900

    Added TODO to a function that seems to be unused services.js

--- public/js/services.js
+++ public/js/services.js
@@ -4,9 +4,12 @@
 
 var smokeymonkeyServices = angular.module('smokeymonkeyServices', ['ngResource']);
 
+// TODO:一食の詳細情報を表示しようとした場合の残骸で現在未使用
 smokeymonkeyServices.factory('Item', ['$resource',
   function($resource){
     return $resource('items/:itemId.json', {}, {
       query: {method:'GET', params:{itemId:'items'}, isArray:true}
     });
   }]);
+
+
